274067fafa080a784fe55f30e79f785715900735
Jiali Zhang <zhangjiali86@gmail.com>
2024-08-20 22:05:22 +0200

    formatting

--- week03/code-lecture/week03lecture/app/src/main/java/lecture03/ReadersWritersSemaphore.java
+++ week03/code-lecture/week03lecture/app/src/main/java/lecture03/ReadersWritersSemaphore.java
@@ -9,11 +9,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReadersWritersSemaphore {
 
-    public ReadersWritersSemaphore() {
+	public ReadersWritersSemaphore() {
 
-		FairReadWriteMonitor m  = new FairReadWriteMonitor();
-		Semaphore semReaders    = new Semaphore(5,true);
-		Semaphore semWriters    = new Semaphore(5,true);
+		FairReadWriteMonitor m = new FairReadWriteMonitor();
+		Semaphore semReaders = new Semaphore(5, true);
+		Semaphore semWriters = new Semaphore(5, true);
 		AtomicInteger noReaders = new AtomicInteger(0);
 		AtomicInteger noWriters = new AtomicInteger(0);
 
@@ -23,32 +23,43 @@ public class ReadersWritersSemaphore {
 
 			// start a reader
 			new Thread(() -> {
-					m.readLock();
-					try{semReaders.acquire();}catch(InterruptedException e){e.printStackTrace();System.exit(-1);}
-					// Note that it always prints less than 6 readers (do not mind the printing order)
-					System.out.println("There are " + noReaders.incrementAndGet() + " threads reading");
-					// read
-					semReaders.release();
-					noReaders.decrementAndGet();
-					m.readUnlock();
+				m.readLock();
+				try {
+					semReaders.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					System.exit(-1);
+				}
+				// Note that it always prints less than 6 readers (do not mind the printing
+				// order)
+				System.out.println("There are " + noReaders.incrementAndGet() + " threads reading");
+				// read
+				semReaders.release();
+				noReaders.decrementAndGet();
+				m.readUnlock();
 			}).start();
 
 			// start a writer
 			new Thread(() -> {
-					m.writeLock();
-					try{semWriters.acquire();}catch(InterruptedException e){e.printStackTrace();System.exit(-1);}
-					System.out.println("There are " + noWriters.incrementAndGet() + " threads writing");
-					// write
-					semWriters.release();
-					noWriters.decrementAndGet();
-					m.writeUnlock();
+				m.writeLock();
+				try {
+					semWriters.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					System.exit(-1);
+				}
+				System.out.println("There are " + noWriters.incrementAndGet() + " threads writing");
+				// write
+				semWriters.release();
+				noWriters.decrementAndGet();
+				m.writeUnlock();
 			}).start();
-	    
-		}
-    }
 
-    public static void main(String[] args) {
+		}
+	}
+
+	public static void main(String[] args) {
 		new ReadersWritersSemaphore();
-    }
-    
+	}
+
 }
