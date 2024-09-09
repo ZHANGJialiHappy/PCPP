9d22805f08f3c12d8a2b3f96a7fb8f980fe2cd87
Jiali Zhang <zhangjiali86@gmail.com>
2024-09-09 08:48:30 +0200

    explanation of Unsafeinitialization

--- week03/code-lecture/week03lecture/app/src/main/java/lecture03/UnsafeInitialization.java
+++ week03/code-lecture/week03lecture/app/src/main/java/lecture03/UnsafeInitialization.java
@@ -31,7 +31,11 @@ class TestUnsafeInitialization {
 
 class UnsafeInitialization {
 	// private volatile int x;
+	// volatile 关键字保证了变量的可见性和有序性。当一个变量被声明为 volatile 时，它有两个主要效果：
+	// 可见性和禁止重排序，AtomicInteger
 	// private volatile Object o;
+	// 一旦对象的 final 字段被分配了值，并且构造函数完成了，那么其他线程总是可以看到正确的值。
+	// 可见性和不可变性，AtomicReferenceclass
 	private int x;
 	private Object o;
 
